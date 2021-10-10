0f3632f2f3fb70d999f528596dd559e8f8499580
Daniil Demidko <demidko@farpost.com>
2021-10-11 03:40:43 +1000

    Concept

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,32 @@
+import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.message
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ChatId.Companion.fromChannelUsername
 import com.github.kotlintelegrambot.entities.ChatId.Companion.fromId
+import java.io.File
 import java.lang.System.getenv
 
 
-class Durov(val token: String, val channel: String) {
 
+/**
+ * Telegram-based embedded database
+ */
+class Durov(val bot: Bot, val channel: ChatId) {
+
+  fun save(description: String, file: File) {
+    bot.sendDocument(channel, file, description);
+  }
+}
+
+/**
+ * Telegram-based embedded database
+ */
+fun Durov(botToken: String, channelName: String): Durov {
+  val bot = bot { token = botToken }
+  val channel = fromChannelUsername(channelName)
+  return Durov(bot, channel)
 }
 
 fun main() {
